5fce84249e9a096275bda09702d922ab1ba02546
Mark Smith <mark.smith@practicalpoetry.co.uk>
2015-07-24 10:00:13 +0100

    Added placeholders for deepish-learning.

--- ish/test_ish.py
+++ ish/test_ish.py
@@ -5,6 +5,7 @@ import pytest
 import ish
 from ish import ish as ish_  # backwards compatibility
 from ish import TRUE_STRINGS
+from ish import HappyIsh, AngryIsh, NeutralIsh, UnIshable
 
 
 def test_true_ish():
@@ -27,3 +28,17 @@ def test_false_ish():
 def test_maybe():
     with pytest.raises(ValueError):
         'Whatever' == True-ish
+
+
+def test_happy_ish():
+    assert isinstance('happy'-ish, HappyIsh)
+
+def test_angry_ish():
+    assert isinstance('angry'-ish, AngryIsh)
+
+def test_neutral_ish():
+    assert isinstance('neutral'-ish, NeutralIsh)
+
+def test_error():
+    with pytest.raises(UnIshable):
+        None == 'flibble'-ish
