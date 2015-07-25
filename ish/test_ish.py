d6c6d207534fe90f0fd5dab7002532782c118267
Sebastian Noack <sebastian.noack@gmail.com>
2015-07-25 14:06:03 +0200

    Refactored/Simplified ish module

--- ish/test_ish.py
+++ ish/test_ish.py
@@ -4,8 +4,7 @@ import pytest
 
 import ish
 from ish import ish as ish_  # backwards compatibility
-from ish import TRUE_STRINGS
-from ish import HappyIsh, AngryIsh, NeutralIsh, UnIshable
+from ish import UnIshable
 
 
 def test_true_ish():
@@ -30,18 +29,6 @@ def test_false_ish():
 def test_maybe():
     with pytest.raises(ValueError):
         'Whatever' == True-ish
-
-
-def test_happy_ish():
-    assert isinstance('happy'-ish, HappyIsh)
-
-
-def test_angry_ish():
-    assert isinstance('angry'-ish, AngryIsh)
-
-
-def test_neutral_ish():
-    assert isinstance('neutral'-ish, NeutralIsh)
 
 
 def test_error():
