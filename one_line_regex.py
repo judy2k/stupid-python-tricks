2a374eed33a8956885bd8ce533fe1e9383a45e5d
Sebastian Noack <sebastian.noack@gmail.com>
2015-11-23 15:35:32 +0000

    Make match_magic() assign to the global scope

--- one_line_regex.py
+++ one_line_regex.py
@@ -9,12 +9,12 @@ def magic_match(pattern, target):
     """
     Match a regex against a target string.
 
-    Assign the result to a 'match' variable in the *caller's scope*.
+    Assign the result to a 'match' variable in the *caller's global scope*.
     """
     frame = sys._getframe(1)
     result = re.match(pattern, target)
     # This is properly, properly evil. Don't do this:
-    frame.f_locals['match'] = result
+    frame.f_globals['match'] = result
     return result
 
 
