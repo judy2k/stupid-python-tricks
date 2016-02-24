f5e62bad81b560dde008c0259d0e55b25613fa32
Mark Smith <mark.smith@practicalpoetry.co.uk>
2016-02-24 11:35:57 +0000

    Something to really mess up pi.

---
+++ gradually_worse_pi.py
@@ -0,0 +1,20 @@
+import sys
+import math as _math
+
+
+class NewMath(object):
+    _pi = _math.pi
+    @property
+    def pi(self):
+        pi = self._pi
+        self._pi += 0.0000001
+        return pi
+
+    def __getattr__(self, name):
+        # Proxy everything else to the real math module
+        return getattr(_math, name)
+
+
+NewMath.__doc__ = _math.__doc__
+
+sys.modules['math'] = NewMath()
