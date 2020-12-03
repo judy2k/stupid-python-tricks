e1bc83fbb9295021d83080b4ac3078756ba4eb1d
Mark Smith <judy@judy.co.uk>
2020-12-03 11:06:57 +0000

    Port to Python 3 and minor refactor of krisskross.

--- decorator_experiments.py
+++ decorator_experiments.py
@@ -3,16 +3,16 @@ import random
 
 
 def once_only(f):
-    '''A function decorater that ensures the decorated function is only called
-       once during the lifetime of the VM.'''
+    """A function decorater that ensures the decorated function is only called
+    once during the lifetime of the VM."""
     static_data = dict(been_called=False, result=None)
 
     @functools.wraps(f)
     def wrapper(*args, **kwargs):
-        if not static_data['been_called']:
-            static_data['result'] = f(*args, **kwargs)
-            static_data['been_called'] = True
-            return static_data['result']
+        if not static_data["been_called"]:
+            static_data["result"] = f(*args, **kwargs)
+            static_data["been_called"] = True
+            return static_data["result"]
 
     return wrapper
 
@@ -22,9 +22,9 @@ class DependencyNotFoundError(NameError):
 
 
 def depends(dependency_list):
-    '''A function decorator that  ensures that the depended-upon functions are
-       called (in order) before the decorated function is called. Dependencies
-       will be called without any arguments.'''
+    """A function decorator that  ensures that the depended-upon functions are
+    called (in order) before the decorated function is called. Dependencies
+    will be called without any arguments."""
 
     def decorator(f):
         @functools.wraps(f)
@@ -37,7 +37,8 @@ def depends(dependency_list):
                         globals()[dependency]()
                     else:
                         raise DependencyNotFoundError(
-                            "Dependency not found: %s" % dependency)
+                            "Dependency not found: %s" % dependency
+                        )
             return f(*args, **kwargs)
 
         return wrapper
@@ -52,18 +53,16 @@ def krisskross(fn):
     """
 
     def wrapped(*args, **kwargs):
-        if random.randint(1, 2) == 1:
-            for k, v in kwargs.items():
-                kwargs[k[::-1]] = v
-                del kwargs[k]
-            return fn(*args[::-1], **kwargs)
+        if random.choice([True, False]):
+            return fn(*args[::-1], **{k[::-1]: v for k, v in kwargs.items()})
         return fn(*args, **kwargs)
+
     return wrapped
 
 
 @once_only
 def should_only_run_once():
-    print "Function has been run!"
+    print("Function has been run!")
 
 
 should_only_run_once()
@@ -71,50 +70,52 @@ should_only_run_once()
 
 
 def a1():
-    print 'a1'
+    print("a1")
 
 
 def b1():
-    print 'b1'
+    print("b1")
 
 
 @depends([a1, b1])
 def c1():
-    print 'c1'
+    print("c1")
 
 
-print '--------'
+print("--------")
 c1()
 
 
 @depends([a1])
 def b2():
-    print 'b2'
+    print("b2")
 
 
 @depends([b2])
 def c2():
-    print 'c2'
+    print("c2")
 
 
-print '--------'
+print("--------")
 c2()
 
 
-@depends(['a1', 'b1'])
+@depends(["a1", "b1"])
 def c3():
-    print 'c3'
+    print("c3")
 
-print '--------'
+
+print("--------")
 c3()
+
 
 @krisskross
 def guess_the_order(*args, **kwargs):
-    print args
-    print kwargs
+    print(args)
+    print(kwargs)
 
 
-guess_the_order(1,2,3, hello='world')
-guess_the_order(1,2,3, hello='world')
-guess_the_order(1,2,3, hello='world')
-guess_the_order(1,2,3, hello='world')
+guess_the_order(1, 2, 3, hello="world")
+guess_the_order(1, 2, 3, hello="world")
+guess_the_order(1, 2, 3, hello="world")
+guess_the_order(1, 2, 3, hello="world")
