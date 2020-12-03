81819a053237b5bcea96c72171f559716f540179
Mark Smith <judy@judy.co.uk>
2020-12-03 12:22:51 +0000

    Port stupid_metaclass_tricks.py to Python 3.

--- stupid_metaclass_tricks.py
+++ stupid_metaclass_tricks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 
+
 class AutoPropertyMeta(type):
     def __init__(cls, name, bases, attrd):
         # Find all getter methods:
-        for prop_name in [name[4:] for name in attrd if
-                          name.startswith('get_')]:
+        for prop_name in [name[4:] for name in attrd if name.startswith("get_")]:
             # Obtain references to the getter and optionally setter methods:
-            getter = attrd.get('get_' + prop_name)
-            setter = attrd.get('set_' + prop_name, None)
+            getter = attrd.get("get_" + prop_name)
+            setter = attrd.get("set_" + prop_name, None)
 
             # Create a new property using the getter and optional setter:
             prop = property(getter, setter)
@@ -16,53 +16,51 @@ class AutoPropertyMeta(type):
             # the class:
             attrd[prop_name] = prop
             setattr(cls, prop_name, prop)
-            delattr(cls, 'get_' + prop_name)
+            delattr(cls, "get_" + prop_name)
             if setter:
-                delattr(cls, 'set_' + prop_name)
+                delattr(cls, "set_" + prop_name)
 
         return super(AutoPropertyMeta, cls).__init__(name, bases, attrd)
 
 
-class AutoProperty:
-    # Apply the magic property-applying metaclass:
-    __metaclass__ = AutoPropertyMeta
+class AutoProperty(metaclass=AutoPropertyMeta):
+    pass
 
 
 class MyRecord(AutoProperty):
-    """ This class magically has its get and set methods replaced with
+    """This class magically has its get and set methods replaced with
     properties, because its metaclass is AutoPropertyMeta:
 
     # Say hello to Bernard:
     >>> bernard = MyRecord('Bernard', 'H', 'Fitch')
 
     # Print out the full name:
-    >>> print bernard
+    >>> print(bernard)
     Bernard H. Fitch
 
     # We now have a read-only property, 'first':
-    >>> print bernard.first
+    >>> print(bernard.first)
     Bernard
 
     # The defined 'get_first' method has disappeared:
-    >>> print bernard.get_first()
+    >>> print(bernard.get_first())
     Traceback (most recent call last):
         ...
     AttributeError: 'MyRecord' object has no attribute 'get_first'
 
     # We can also access Bernard's middle initial:
-    >>> print bernard.initial
+    >>> print(bernard.initial)
     H
 
     # Because there's a setter, we can also set the initial:
     >>> bernard.initial = 'J'
 
     # ... and let's just make sure that's changed the value of initial:
-    >>> print bernard
+    >>> print(bernard)
     Bernard J. Fitch
 
     # And if we do a 'dir' on Bernard, we can't see any magic:
-    >>> print sorted( \
-            [attr for attr in dir(bernard) if not attr.startswith('_')])
+    >>> print(sorted([attr for attr in dir(bernard) if not attr.startswith('_')]))
     ['first', 'initial', 'last']
     """
 
@@ -87,7 +85,7 @@ class MyRecord(AutoProperty):
         return "%s %s. %s" % (self._first, self._initial, self._last)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import doctest
 
     doctest.testmod()
