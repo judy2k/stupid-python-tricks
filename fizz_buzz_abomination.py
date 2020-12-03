5cb3dadf1f6de4c9d576d9586b4c1424fe872071
Mark Smith <judy@judy.co.uk>
2020-12-03 11:25:55 +0000

    Port fizz_buzz_abomination.py

--- fizz_buzz_abomination.py
+++ fizz_buzz_abomination.py
@@ -20,12 +20,16 @@ def divisible_by(divisor):
     """
     Return a function that tests if a number is divisible by `divisor`.
     """
+
     def is_divisible(n):
         """ Placeholder docstring. """
         return n % divisor == 0
+
     is_divisible.__doc__ = """
         Return True if `n` is divisible by {}.
-    """.format(divisor)
+    """.format(
+        divisor
+    )
     return is_divisible
 
 
@@ -38,7 +42,7 @@ def identifiericate(s):
     """
     Convert a string to a valid Python identifier.
     """
-    return re.sub(r'[^a-z0-9_]', '', re.sub(r'[ \-]', '_', str(s).lower()))
+    return re.sub(r"[^a-z0-9_]", "", re.sub(r"[ \-]", "_", str(s).lower()))
 
 
 def returner(val):
@@ -53,10 +57,13 @@ def returner(val):
     def i_will_return(n):
         """ Placeholder docstring. """
         return val
+
     i_will_return.__name__ = funcname
     i_will_return.__doc__ = """
         A function that returns {!r}
-    """.strip().format(val)
+    """.strip().format(
+        val
+    )
 
     return i_will_return
 
@@ -66,11 +73,13 @@ class UnFizzBuzzable(Exception):
     An exception raised by a fizz-buzz strategy if it cannot resolve the
     provided number.
     """
+
     def __init__(self, n, test_result, lookup):
         msg = """I don't know what to do with n={}.
 Test result is {}.
 Rules are {}""".format(
-            n, test_result, lookup)
+            n, test_result, lookup
+        )
         super(UnFizzBuzzable, self).__init__(msg)
 
 
@@ -79,6 +88,7 @@ class FizzBuzz(object):
     An infinite iterable (and callable) that executes a given
     FizzBuzz strategy.
     """
+
     def __init__(self, strategy):
         self._strategy = strategy
 
@@ -98,9 +108,11 @@ class FizzBuzz(object):
 
 def fizz_buzz(strategy):
     if isclass(strategy):
+
         @wraps(strategy)
         def fizz_buzz_creator(*args, **kwargs):
             return FizzBuzz(strategy(*args, **kwargs))
+
         return fizz_buzz_creator
     else:
         return FizzBuzz(strategy)
@@ -129,6 +141,7 @@ class FizzBuzzLookupStrategy(object):
     A FizzBuzz strategy implemented by looking up handlers in a provided
     truth-table.
     """
+
     def __init__(self, tests, rules):
         self._tests = tests
         self._lookup = {}
@@ -136,8 +149,7 @@ class FizzBuzzLookupStrategy(object):
             self.add_rule(spec, handler)
 
     def add_rule(self, spec, handler):
-        for key in product(
-                *([True, False] if x is None else [x] for x in spec)):
+        for key in product(*([True, False] if x is None else [x] for x in spec)):
             self._lookup[key] = handler
 
     def __call__(self, n):
@@ -155,13 +167,13 @@ class FizzBuzzLookupStrategy(object):
 
     def __str__(self):
         result = StringIO()
-        print >>result, "FizzBuzzLookupStrategy ["
+        print("FizzBuzzLookupStrategy [", file=result)
         rules = [(k, v) for k, v in self._lookup.items()]
         rules.sort(key=lambda r: r[0])
         for key, handler in rules:
-            criteria = ' '.join(['T' if test else '.' for test in key])
-            print "{}    {!r}".format(criteria, handler)
-        print >>result, "]"
+            criteria = " ".join(["T" if test else "." for test in key])
+            print("{}    {!r}".format(criteria, handler))
+        print("]", file=result)
         return result.getvalue()
 
 
@@ -173,7 +185,7 @@ fizz_buzz_lookup = FizzBuzzLookupStrategy(
         ((True, False, False), returner("Fizz!")),
         ((True, True, False), returner("Fizz Buzz!")),
         ((None, None, True), returner("Oh Yes!")),
-    ]
+    ],
 )
 
 
@@ -182,7 +194,7 @@ def main():
     Print out FizzBuzz values every quarter-second, forever.
     """
     arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument('-v', '--verbose', action='store_true')
+    arg_parser.add_argument("-v", "--verbose", action="store_true")
 
     arguments = arg_parser.parse_args()
 
@@ -192,10 +204,14 @@ def main():
     # fizz_buzz_elif = FizzBuzz(fizz_buzz_elif)
 
     import time
-    for n, v in enumerate(fizz_buzz_lookup, start=1):
-        print "{}: {}".format(n, v)
-        time.sleep(0.25)
+
+    try:
+        for n, v in enumerate(fizz_buzz_lookup, start=1):
+            print("{}: {}".format(n, v))
+            time.sleep(0.25)
+    except KeyboardInterrupt as interrupt:
+        print("\nAww, you interrupted me!")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
